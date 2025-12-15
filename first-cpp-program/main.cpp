96cdd7d159b072c8f691876186a88ae569918c20
manrajwazir00 <manrajwazir37@gmail.com>
2025-12-15 15:47:50 -0700

    added use of std::cin

--- first-cpp-program/main.cpp
+++ first-cpp-program/main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-    std::cout << "Hello, world!";
+    std::cout << "Hello, world!" << std::endl; // print a greeting message
+    std::cout << 5 << '\n';                    // using \n is better because it doesnt flush like endl
+    std::cout << -6.7;                         // print the literal number `-6.7`
+    std::cout << 'H';                          // print the literal character `H`
+    std::cout << "Hello" << "\n";              // print the literal text `Hello`
+
+    std::cout << "Enter a number: ";
+    int x{};
+    std::cin >> x;
+    std::cout << "You entered " << x << " bombaclatt" << '\n';
     return 0;
 }
